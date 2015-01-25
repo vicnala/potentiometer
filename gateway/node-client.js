3730552db45ae0400727e2ea87cf3aa3894484b3
Kyle King <kmking72@gmail.com>
2015-01-25 16:37:16 -0500

    Added comments to Node file

--- gateway/node-client.js
+++ gateway/node-client.js
@@ -1,6 +1,11 @@
-var DDPClient = require("ddp");
-var currentPort = "/dev/cu.usbmodem1421";
+// Name serial port - there should be a smarter way to do this, but this seems easiest
+// var currentPort = "/dev/ttyACM0"; // A PC serial port
+var currentPort = "/dev/cu.usbmodem" + "1411"; // direct left port
+// var currentPort = "/dev/cu.usbmodem" + "1421"; // direct right port
 
+var DDPClient = require("ddp");
+
+// Connect to Meteor
 var ddpclient = new DDPClient({
   host: "localhost",
   port: 3000,
@@ -14,16 +19,16 @@ var ddpclient = new DDPClient({
 });
 
 ddpclient.connect(function(error) {
+  // Error Checking
   if (error) {
     console.log('DDP connection error!');
     return;
   }
-
   console.log('connected to Meteor!');
 
+  // Configure serial port
   var serialport = require("serialport");
   var SerialPort = serialport.SerialPort; // localize object constructor
-  // var serialPort = new SerialPort("/dev/ttyACM0", {
   var serialPort = new SerialPort(currentPort, {
     baudrate: 115200,
     // look for return and newline at the end of each data packet:
@@ -32,11 +37,19 @@ ddpclient.connect(function(error) {
 
   function showPortOpen() { console.log('port open. Data rate: ' + serialPort.options.baudRate); }
   function saveLatestData(data) {
-    var spin = data + 10;
-    serialPort.write(spin);
-    // Handle data
-    // console.log(data);
+    // Error Checking
     console.log('data received: ' + data);
+
+    // Push data to Arduino
+    // var spin = data + 10;
+    // serialPort.write(spin);
+    // Copied from the documentation - use at own risk
+    // serialPort.write(spin, function(err, results) {
+    //   console.log('err ' + err);
+    //   console.log('results ' + results);
+    // });
+
+    // Call Meteor actions with "data"
     ddpclient.call('push', [data], function(err, result) {
       console.log('called push function, result: ' + result);
     });
@@ -44,9 +57,12 @@ ddpclient.connect(function(error) {
       console.log('called Loop function, result: ' + result);
     });
   }
+
+  // Error Checking
   function showPortClose() { console.log('port closed.'); }
   function showError(error) { console.log('Serial port error: ' + error); }
 
+  // Node events - trigger specific functions upon specific events
   serialPort.on('open', showPortOpen);
   serialPort.on('data', saveLatestData);
   serialPort.on('close', showPortClose);
