4da70329a5ebd4417dea6ccecf04743cf991bce9
Kyle King <kmking72@gmail.com>
2015-01-26 20:45:31 -0500

    TimeSeries Improvements: Squashed commit of the following:
    commit 60cdeca328155356bdce5859f469107113dcf7bc
    Author: Kyle King <kmking72@gmail.com>
    Date:   Mon Jan 26 20:32:39 2015 -0500
    
        Lots of data!
    
        Faster piping and slimmer code base
    
    commit 4367386d1dae0cda3f9d36cde33eed516dd45e12
    Author: Kyle King <kmking72@gmail.com>
    Date:   Mon Jan 26 20:26:52 2015 -0500
    
        Removed Serial.Write code
    
        Moved all code to the feature branch for node -> arduino. Now only code for arduino -> node -> meteor
    
    commit 812fc2326c1cd03ece835dd6ea4750ef82437bab
    Author: Kyle King <kmking72@gmail.com>
    Date:   Mon Jan 26 19:49:36 2015 -0500
    
        Compact code - field as object and html each
    
    commit 1ecbf2f10f58aa3035fb1ddcdede177ee09c4087
    Author: Kyle King <kmking72@gmail.com>
    Date:   Mon Jan 26 19:31:55 2015 -0500
    
        Fixed file cleanup
    
    commit 207d5c0c7d1f1380517ec28334bbe032b4f36429
    Author: Kyle King <kmking72@gmail.com>
    Date:   Mon Jan 26 19:09:25 2015 -0500
    
        Restructured app
    
    commit 3adfcd2fb63da318d94f070e48fe2c2194341573
    Author: Kyle King <kmking72@gmail.com>
    Date:   Sun Jan 25 18:00:29 2015 -0500
    
        Code clean up
    
    commit dbc0b13cebad4377957eff2677f11cdcfabc2bcb
    Author: Kyle King <kmking72@gmail.com>
    Date:   Sun Jan 25 17:53:57 2015 -0500
    
        Basic csv parsing
    
        Testing csv parsing and passing counter variable over serial connection rather than waiting to reach a database. This is a median step to use date/time formats
        * Note: seem to only call one update call to mongo per meteor method
    
    commit 33f0c75af9cb09a0fd45e58961a400fbdf177f78
    Author: Kyle King <kmking72@gmail.com>
    Date:   Sun Jan 25 17:03:49 2015 -0500
    
        Comments + consolidated code
    
    commit 3730552db45ae0400727e2ea87cf3aa3894484b3
    Author: Kyle King <kmking72@gmail.com>
    Date:   Sun Jan 25 16:37:16 2015 -0500
    
        Added comments to Node file
    
    commit fe098f6382f88d5bc4fe533d689fa47f777e0a38
    Author: Kyle King <kmking72@gmail.com>
    Date:   Thu Jan 22 19:36:47 2015 -0500
    
        Testing array data structure
    
        Succesfully stored past data, but the counter variable isn't updating with each new piece of data
    
    commit c6bd50c8b47c9809b252844f173ebf52bda69868
    Author: Kyle King <kmking72@gmail.com>
    Date:   Thu Jan 22 17:20:29 2015 -0500
    
        test: 8 sub-value objects
    
        displays 8 data points, but so far only updates one and isn't storing past data
    
    commit 1b486aba65ee13046719a1cbda995a7008e48f6d
    Author: Kyle King <kmking72@gmail.com>
    Date:   Sun Jan 18 12:20:58 2015 -0500
    
        Updated ReadMe for new code

--- node-client.js
+++ node-client.js
@@ -1,5 +1,12 @@
+// Name serial port - there should be a smarter way to do this, but this seems easiest
+// var currentPort = "/dev/ttyACM0"; // A PC serial port
+// var currentPort = "/dev/cu.usbmodem" + "1411"; // direct left port
+// var currentPort = "/dev/cu.usbmodem" + "1421"; // direct right port
+var currentPort = "/dev/cu.usbmodem" + "14211"; // indirect right port: closest to aux power
+
 var DDPClient = require("ddp");
 
+// Connect to Meteor
 var ddpclient = new DDPClient({
   host: "localhost",
   port: 3000,
@@ -13,17 +20,17 @@ var ddpclient = new DDPClient({
 });
 
 ddpclient.connect(function(error) {
+  // Error Checking
   if (error) {
     console.log('DDP connection error!');
     return;
   }
-
   console.log('connected to Meteor!');
 
+  // Configure serial port
   var serialport = require("serialport");
   var SerialPort = serialport.SerialPort; // localize object constructor
-  // var serialPort = new SerialPort("/dev/ttyACM0", {
-  var serialPort = new SerialPort("/dev/cu.usbmodem1411", {
+  var serialPort = new SerialPort(currentPort, {
     baudrate: 115200,
     // look for return and newline at the end of each data packet:
     parser: serialport.parsers.readline("\r\n")
@@ -31,18 +38,25 @@ ddpclient.connect(function(error) {
 
   function showPortOpen() { console.log('port open. Data rate: ' + serialPort.options.baudRate); }
   function saveLatestData(data) {
-    var spin = data + 10;
-    serialPort.write(spin);
-    // Handle data
-    // console.log(data);
+    // See what data comes through
     console.log('data received: ' + data);
-    ddpclient.call('push', [data], function(err, result) {
-      console.log('called function, result: ' + result);
+    var array = data.split(','); // CSV Data Parse:
+    // Print each parsed data
+    for (var i = 0; i < array.length; i++) {
+       console.log('data point ' + i + ' parsed: ' + array[i]);
+    }
+
+    // Call Meteor actions with "data"
+    ddpclient.call('loop', [array], function(err, result) {
+      console.log('called Loop function, result: ' + result);
     });
   }
+
+  // Error Checking
   function showPortClose() { console.log('port closed.'); }
   function showError(error) { console.log('Serial port error: ' + error); }
 
+  // Node events - trigger specific functions upon specific events
   serialPort.on('open', showPortOpen);
   serialPort.on('data', saveLatestData);
   serialPort.on('close', showPortClose);
