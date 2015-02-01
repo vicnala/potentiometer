15824f244b42720a786f93ccf4ba06d63e0164e3
Kyle King <kmking72@gmail.com>
2015-02-01 13:14:17 -0500

    Per bike schema with session variable selector

--- meteor/potentiometer.js
+++ meteor/potentiometer.js
@@ -4,7 +4,8 @@ TimeSeries = new Meteor.Collection('timeseries'); // Time series data
 if (Meteor.isClient) {
   Template.timeseries.helpers({
     timeseries: function () {
-      return TimeSeries.findOne();
+      Session.set("currentBike", 4);
+      return TimeSeries.findOne({Bike: Session.get("currentBike")});
     }
   });
 }
@@ -12,31 +13,39 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   // Insert database for first commit
   if (TimeSeries.find().count() === 0) {
-    TimeSeries.insert({
-      counter: 0,
-      value: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    });
+    for (var i = 0; i < 10; i++) {
+      var filler = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+      TimeSeries.insert({
+        Bike: i,
+        Lat: filler,
+        Long: filler
+      });
+    };
   }
 
   Meteor.methods({
-    'loop': function (value) {
+    'loop': function (cleanArray, schema) {
+      for (var i = 0; i < cleanArray.length; i++) {
+        console.log(i + ' = ' + schema[i] + ' : ' + cleanArray[i]);
+      }
       // Prepare to udpate MongoDB
       var fields = {};
-      fields["value." + value[0]] = value[1];
-      fields["counter"] = value[0];
+      console.log("value." + cleanArray[0]);
+      fields["value." + cleanArray[0]] = cleanArray[1];
+      fields["counter"] = cleanArray[0];
 
       // Update MongoDB data
-      // var record = TimeSeries.findOne();
-      // TimeSeries.update(
-      //   record,
-      //   { $set: fields }
-      // );
+      var record = TimeSeries.findOne();
+      TimeSeries.update(
+        record,
+        { $set: fields }
+      );
 
       // Check values
-      // console.log("Node Value: " + value); // piped to shell
-      // console.log("counter: " + record.value[1]); // piped to shell
+      // console.log("Node Value: " + cleanArray); // piped to shell
+      // console.log("counter: " + record.cleanArray[1]); // piped to shell
       // console.log("counter: " + counter); // piped to shell
-      console.log("data: " + value[1]); // piped to shell
+      console.log("data: " + cleanArray[1]); // piped to shell
       return "ok";
     }
   });
