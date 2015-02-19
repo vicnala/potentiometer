9dc83963c1190bd00e8d44aeb8444d06cedfb726
Kyle King <kmking72@gmail.com>
2015-02-18 22:31:30 -0500

    Squashed commit of the following:
    commit 5a10eb3e2d0d7d92a98e490698f4f216c843b9e7
    Author: Kyle King <kmking72@gmail.com>
    Date:   Wed Feb 18 22:31:16 2015 -0500
    
        Switched to highstock timeseries
    
    commit 0e84c33dbfd747021e5ad9ef5a77cf4828f6a693
    Author: Kyle King <kmking72@gmail.com>
    Date:   Wed Feb 18 22:10:57 2015 -0500
    
        Back to working version

--- meteor/client/lineDemo.js
+++ meteor/client/lineDemo.js
@@ -1,3 +1,70 @@
+function buildSpline(splineData) {
+    $('#container-spline').highcharts('StockChart', {
+        // chart : {
+        //     events : {
+        //         load : function () {
+
+        //             // set up the updating of the chart each second
+        //             var series = this.series[0];
+        //             setInterval(function () {
+        //                 var x = (new Date()).getTime(), // current time
+        //                     y = Math.round(Math.random() * 100);
+        //                 series.addPoint([x, y], true, true);
+        //             }, 1000);
+        //         }
+        //     }
+        // },
+
+        rangeSelector: {
+            buttons: [{
+                count: 1,
+                type: 'minute',
+                text: '1M'
+            }, {
+                count: 5,
+                type: 'minute',
+                text: '5M'
+            }, {
+                type: 'all',
+                text: 'All'
+            }],
+            inputEnabled: false,
+            selected: 0
+        },
+
+        title : {
+            text : 'Live random data'
+        },
+
+        exporting: {
+            enabled: false
+        },
+
+        series: [{
+            name: splineData.name,
+            data: splineData.data
+        }]
+        // series : [{
+        //     name : 'Random data',
+        //     data : (function () {
+        //         // generate an array of random data
+        //         var data = [], time = (new Date()).getTime(), i;
+
+        //         for (i = -999; i <= 0; i += 1) {
+        //             data.push([
+        //                 time + i * 1000,
+        //                 Math.round(Math.random() * 100)
+        //             ]);
+        //         }
+        //         console.log(data[0]);
+        //         return data;
+        //     }())
+        // }]
+    });
+
+}
+
+
 // Not used, but may need to truncate values of graph
 // // .last(num) call to slice array for given length
 // if (!Array.prototype.last){
@@ -6,52 +73,52 @@
 //     };
 // }
 
-function buildSpline(splineData) {
-  $('#container-spline').highcharts({
-      chart: {
-          type: 'spline',
-          animation: Highcharts.svg, // don't animate in old IE
-          marginRight: 10,
-          zoomType: 'x',
-          panning: true,
-          panKey: 'shift'
-      },
-      title: {
-          text: 'Arduino Piped Potentiometer data'
-      },
-      xAxis: {
-          type: 'datetime',
-          tickPixelInterval: 150
-      },
-      yAxis: {
-          title: {
-              text: 'Value'
-          },
-          plotLines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
-          }]
-      },
-      tooltip: {
-          formatter: function () {
-              return '<b>' + this.series.name + '</b><br/>' +
-                  Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                  Highcharts.numberFormat('P-Value: ', this.y, 2);
-          }
-      },
-      legend: {
-          enabled: false
-      },
-      exporting: {
-          enabled: false
-      },
-      series: [{
-          name: splineData.name,
-          data: splineData.data
-      }]
-  });
-}
+// function buildSpline(splineData) {
+//   $('#container-spline').highcharts({
+//       chart: {
+//           type: 'spline',
+//           animation: Highcharts.svg, // don't animate in old IE
+//           marginRight: 10,
+//           zoomType: 'x',
+//           panning: true,
+//           panKey: 'shift'
+//       },
+//       title: {
+//           text: 'Arduino Piped Potentiometer data'
+//       },
+//       xAxis: {
+//           type: 'datetime',
+//           tickPixelInterval: 150
+//       },
+//       yAxis: {
+//           title: {
+//               text: 'Value'
+//           },
+//           plotLines: [{
+//               value: 0,
+//               width: 1,
+//               color: '#808080'
+//           }]
+//       },
+//       tooltip: {
+//           formatter: function () {
+//               return '<b>' + this.series.name + '</b><br/>' +
+//                   Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+//                   Highcharts.numberFormat('P-Value: ', this.y, 2);
+//           }
+//       },
+//       legend: {
+//           enabled: false
+//       },
+//       exporting: {
+//           enabled: false
+//       },
+//       series: [{
+//           name: splineData.name,
+//           data: splineData.data
+//       }]
+//   });
+// }
 
 // Call the function to built the chart when the template is rendered
 Template.lineDemo.rendered = function() {
@@ -73,6 +140,8 @@ Template.lineDemo.rendered = function() {
           Highcharts.charts[0].series[0].addPoint([x , y], true, true);
           // If you want to play around with adding new points from the console:
           // Highcharts.charts[0].series[0].addPoint([Highcharts.charts[0].series[0].data[19].x + 1000, Highcharts.charts[0].series[0].data[19].y*Math.random()+12], true, true)
+          // For highstock
+          // Highcharts.charts[0].series[0].addPoint([Highcharts.charts[0].series[0].data[999].x + 1000, Highcharts.charts[0].series[0].data[999].y*Math.random()+12], true, true)
         }
       });
     });
